2d9cea48de8e328f6c6614df21fc52195ab83c60
Nomagno <alvarosaez316@gmail.com>
2026-02-06 15:01:15 +0100

    Make fastest lap and last lap messages work properly in splitscreen

--- src/states_screens/race_gui_base.cpp
+++ src/states_screens/race_gui_base.cpp
@@ -270,7 +270,7 @@ void RaceGUIBase::drawAllMessages(const Kart* kart,
 
     // Draw less important messages first, at the very bottom of the screen
     // unimportant messages are skipped in multiplayer, they take too much screen space
-    if (RaceManager::get()->getNumLocalPlayers() < 2 &&
+    if (/*RaceManager::get()->getNumLocalPlayers() < 2 &&*/
         !m_ignore_unimportant_messages)
     {
         for (AllMessageType::const_iterator i = m_messages.begin();
